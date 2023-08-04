d4285bc65f8ad67a3a80b62269d143f5d8f81159
crestaa <gabricre01@gmail.com>
2023-08-04 15:29:50 +0200

    feat: base code for reading data + mockup

--- src/enddevice/enddevice.ino
+++ src/enddevice/enddevice.ino
@@ -31,23 +31,33 @@
 #define LORA_FIX_LENGTH_PAYLOAD_ON                  false
 #define LORA_IQ_INVERSION_ON                        false
 
-#define BUFFER_SIZE                                 128 // Define the payload size here
+#define BUFFER_SIZE                                 128       // LoRa payload size, same for Tx and Rx
+
+#define PIN_HUM 0
+#define PIN_TEMP 0
+
 
 char txpacket[BUFFER_SIZE];
-
 double txNumber;
-
 bool lora_idle=true;
-
 static RadioEvents_t RadioEvents;
+char mac[18];
+
 void OnTxDone( void );
 void OnTxTimeout( void );
+void sendLoRaData(float value, const char* field);
+void readHum( void );
+void readData( void );
 
 void setup() {
   Serial.begin(115200);
   Mcu.begin();
 
   txNumber=0;
+
+  uint8_t mac_addr[6];
+  WiFi.macAddress(mac_addr);
+  snprintf(mac, sizeof(mac), "%02X:%02X:%02X:%02X:%02X:%02X", mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
 
   RadioEvents.TxDone = OnTxDone;
   RadioEvents.TxTimeout = OnTxTimeout;
@@ -65,43 +75,38 @@ void loop()
 {
 	if(lora_idle == true)
 	{
-    delay(1000);
-
-    sendLoRaData(29.5, "temp");
+    delay(5000);
+    readData();
     lora_idle = false;
 	}
   Radio.IrqProcess( );
 }
 
-void OnTxDone( void )
-{
-	Serial.println("TX done......");
-	lora_idle = true;
+void readData(){
+  readHum();
+  delay(400);
+  readTemp();
 }
 
-void OnTxTimeout( void )
-{
-    Radio.Sleep( );
-    Serial.println("TX Timeout......");
-    lora_idle = true;
+void readHum(){
+  // MOCKUP
+  sendLoRaData(55, "hum");
+}
+void readTemp(){
+  // MOCKUP
+  sendLoRaData(29.5, "temp");
 }
 
-
-
-void sendLoRaData(float field, const char* read) {
-  // MAC addr + random ID generation
-  uint8_t mac[6];
-  WiFi.macAddress(mac);
-  char macStr[18];
-  snprintf(macStr, sizeof(macStr), "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+void sendLoRaData(float value, const char* field) {
+  // random ID generation
   int id = random(100000);
 
   // JSON object
   StaticJsonDocument<200> jsonDocument;
-  jsonDocument["mac"] = macStr;
+  jsonDocument["mac"] = mac;
   jsonDocument["id"] = id;
+  jsonDocument["value"] = value;
   jsonDocument["field"] = field;
-  jsonDocument["read"] = read;
 
   // serialized JSON to string
   String jsonString;
@@ -112,4 +117,20 @@ void sendLoRaData(float field, const char* read) {
 
   Serial.print("Dati JSON trasmessi: ");
   Serial.println(txpacket);
+}
+
+
+
+
+
+void OnTxDone( void )
+{
+	Serial.println("TX done......");
+	lora_idle = true;
+}
+void OnTxTimeout( void )
+{
+    Radio.Sleep( );
+    Serial.println("TX Timeout......");
+    lora_idle = true;
 }
