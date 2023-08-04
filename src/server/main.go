96f828a6d52352cc5e6ea90e90218a78a9d2b87b
Gabriele Crestanello <gabriele.crestanello@studio.unibo.it>
2023-08-04 17:33:08 +0200

    feat: server parse

--- src/server/main.go
+++ src/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -10,31 +11,55 @@ import (
 )
 
 func main() {
+	// MQTT broker setup
 	opts := MQTT.NewClientOptions().AddBroker(MQTT_BROKER)
 	opts.SetClientID("mqtt-subscriber")
 	opts.SetUsername(MQTT_USER)
 	opts.SetPassword(MQTT_PASS)
 
+	// message handling logic
 	opts.SetDefaultPublishHandler(func(client MQTT.Client, msg MQTT.Message) {
-		fmt.Printf("Nuovo messaggio ricevuto sul topic: %s - Messaggio: %s\n", msg.Topic(), msg.Payload())
+		handleMessage(msg)
 	})
 
 	client := MQTT.NewClient(opts)
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
 		log.Fatal(token.Error())
 	}
-
 	topic := MQTT_TOPIC
 	if token := client.Subscribe(topic, 0, nil); token.Wait() && token.Error() != nil {
 		log.Fatal(token.Error())
 	}
 
-	fmt.Println("In attesa di messaggi...")
+	fmt.Println("Waiting for messages...")
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	<-c
 
 	client.Disconnect(250)
-	fmt.Println("Disconnesso.")
+	fmt.Println("Disconnected.")
+}
+
+type Message struct {
+	MAC   string  `json:"mac"`
+	ID    int     `json:"id"`
+	Value float64 `json:"value"`
+	Type  string  `json:"type"`
+}
+
+func handleMessage(msg MQTT.Message) {
+	_, err := parseJSON(msg)
+	if err != nil {
+		fmt.Println("ERR during JSON parsing:", err)
+		return
+	}
+
+}
+
+func parseJSON(msg MQTT.Message) (Message, error) {
+	var messageData Message
+	err := json.Unmarshal(msg.Payload(), &messageData)
+
+	return messageData, err
 }
