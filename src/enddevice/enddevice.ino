d48b31047623596bc3e6d938f8f7c03dcf6042e7
crestaa <gabricre01@gmail.com>
2023-08-16 14:01:22 +0200

    feat: tweaks for longer distance on LoRa

--- src/enddevice/enddevice.ino
+++ src/enddevice/enddevice.ino
@@ -16,18 +16,18 @@
 
 #define RF_FREQUENCY                                868000000 // Hz
 
-#define TX_OUTPUT_POWER                             5        // dBm
+#define TX_OUTPUT_POWER                             14        // dBm
 
 #define LORA_BANDWIDTH                              0         // [0: 125 kHz,
                                                               //  1: 250 kHz,
                                                               //  2: 500 kHz,
                                                               //  3: Reserved]
-#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
+#define LORA_SPREADING_FACTOR                       12         // [SF7..SF12]
 #define LORA_CODINGRATE                             1         // [1: 4/5,
                                                               //  2: 4/6,
                                                               //  3: 4/7,
                                                               //  4: 4/8]
-#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+#define LORA_PREAMBLE_LENGTH                        12         // Same for Tx and Rx
 #define LORA_SYMBOL_TIMEOUT                         0         // Symbols
 #define LORA_FIX_LENGTH_PAYLOAD_ON                  false
 #define LORA_IQ_INVERSION_ON                        false
@@ -102,7 +102,8 @@ void readTemp(){
 void sendLoRaData(float value, const char* type) {
   // random ID generation
   int id = random(100000);
-
+  //int id = txNumber;
+  txNumber++;
   // JSON object
   StaticJsonDocument<200> jsonDocument;
   jsonDocument["mac"] = mac;
